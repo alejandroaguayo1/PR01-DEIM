b5afaaa3fb5f8417c6cbea1d9588d743c514ca55
AlejandroAguayo <alejandro77tcm@gmail.com>
2021-09-28 09:49:14 +0200

    ejercicio 3b
    calculadora

--- Ejercicios Alejandro Aguayo 21-9-2021/Assets/Scripts/ejercicio3b.cs
+++ Ejercicios Alejandro Aguayo 21-9-2021/Assets/Scripts/ejercicio3b.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ejercicio : MonoBehaviour
 {
+    [SerializeField] float n1= 10;
+        [SerializeField] float n2= 10;
+        [SerializeField] string op;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Calcular(125.6f, 587f, "dividir"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Calcular(n1, n2, "op");
+
+        }
+    }
+
+    void Calcular(float num1, float num2, string op)
+    {
+        float resultado = 0f;
+        if(op == "dividir")
+        {
+            resultado = num1 / num2;
+        }
+        else if(op == "restar")
+        {
+            resultado = num1 - num2;
+        }
+        else if (op == "sumar")
+        {
+            resultado = num1 + num2;
+        }
+        else if (op == "multiplicar")
+        {
+            resultado = num1 * num2;
+        }
+        print(resultado);
     }
 }
